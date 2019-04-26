e410071faed0fbaa80b0ff9777acc5b2728a855c
Pratima Sakinala <pratimasakinala13@gmail.com>
2019-04-25 20:05:26 -0700

    reviewing number variables (#3)

---
+++ js/review-number.js
@@ -0,0 +1,17 @@
+var num1 = 123;
+var num2 = 321;
+
+var sum = num1 + num2;
+console.log(sum);
+
+var diff = num1 - num2;
+console.log(diff);
+
+var mul = num1 * num2;
+console.log(mul);
+
+var div = num1 / num2;
+console.log(div);
+
+var mod = num1 % num2;
+console.log(mod);
