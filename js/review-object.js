fc4ad889e002baf073b43189e3f41d3078bf5e50
Pratima Sakinala <pratimasakinala13@gmail.com>
2019-04-25 19:49:06 -0700

    review of object variable

---
+++ js/review-object.js
@@ -0,0 +1,10 @@
+var fullName = {
+    first: 'Pratima',
+    last: 'Sakinala'
+};
+console.log(fullName);
+console.log(fullName.first);
+console.log(fullName.last);
+
+console.log(fullName.first + ' ' + fullName.last);
+console.log(fullName.first.concat(' ', fullName.last));
